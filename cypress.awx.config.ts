ecba7b2e34a9a9450f0c18be2c38d5b7eecba680
jerabekjiri <jerabekjirka@email.cz>
2024-06-05 14:25:44 +0200

    run only wf approvals

--- cypress.awx.config.ts
+++ cypress.awx.config.ts
@@ -5,7 +5,9 @@ import { baseConfig } from './cypress.base.config';
 
 baseConfig.e2e!.specPattern = 'cypress/e2e/awx/**/*.cy.ts';
 baseConfig.e2e!.baseUrl = 'https://localhost:4101';
-baseConfig.component!.specPattern = 'frontend/awx/**/*.cy.{js,jsx,ts,tsx}';
+// baseConfig.component!.specPattern = 'frontend/awx/**/*.cy.{js,jsx,ts,tsx}';
+baseConfig.e2e!.specPattern = 'cypress/e2e/awx/administration/wfApprovals.cy.ts';
+
 setValue(baseConfig, 'component.devServer.webpackConfig.devServer.port', 4201);
 
 module.exports = defineConfig(baseConfig);
